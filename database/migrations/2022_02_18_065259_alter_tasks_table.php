faf021c5bc19a00fca3b7ff000af872dc5684bc1
Felix Ouma <fatonoh@gmail.com>
2022-02-18 10:52:50 +0300

    Add due date when creating a task

---
+++ database/migrations/2022_02_18_065259_alter_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->date('due_date')->default(date("2022-05-20"));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('due_date');
+        });
+    }
+}
