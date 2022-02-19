aadcd2e1afa713a23a09fcf525b02d877ad9e199
Felix Ouma <fatonoh@gmail.com>
2022-02-19 04:52:27 +0300

    Change layout of homepage and add images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    ProjectTrackerApp
+                    {{ __('Project Tracker') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -42,10 +42,10 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link " href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="btn btn-primary font-weight-bold" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
                             <li class="nav-item dropdown">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,37 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
+@extends('layouts.app')
+@section('content')
+    <div class="home">
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+        </div>
+    </div>
+    <div class="container">
+            <div class="">
+                <div class="content text-center">
+                    <div class="title m-b-md">
+                        {{ __('Project Tracker') }}
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Project Tracker App
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="text-center">
+                    <a class="btn btn-primary font-weight-bold" href="{{ route('login') }}">{{ $users->count() }} {{ __('User(s)') }}</a>
+                    <a class="btn btn-primary font-weight-bold" href="{{ route('register') }}">{{ $projects->count() }} {{ __('Project(s)') }}</a>
+                    
                 </div>
             </div>
+    </div>
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h4 class="font-weight-bold">{{ __('Better organization of project tasks') }}</h4>
+                <img src="/images/task.svg" class="background-image">
+                <p class="text-justify">{{ __('Tracker\'s shared backlog makes priorities clear so the team can stay organized. Easily visualize scope, focus your teamwork, and stay nimble when circumstances change.') }}</p>
+            </div>
+            <div class="col-md-6">
+                <h4 class="font-weight-bold">{{ __('Tools to help you adapt and evolve') }}</h4>
+                <img src="/images/project.svg" class="background-image">
+                <p class="text-justify">{{ __('Tracker\'s guided iteration planning helps you break down and prioritize projects into manageable chunks so the team can keep the momentum toward delivering.') }}</p>
+            
+            </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection()
